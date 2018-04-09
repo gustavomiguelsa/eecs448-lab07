675c516090a06fc548be48e9a10e03622163debe
Gustavo Assuncao <g111s401@ku.edu>
2018-04-09 13:55:44 -0500

    First Commit.

---
+++ testclass.cpp
@@ -0,0 +1,7 @@
+/**
+*	@author Gustavo Assuncao
+*	@date 04/09/2018
+*	@file testclass.cpp
+*/
+
+#include "testclass.h"
---
+++ testclass.h
@@ -0,0 +1,21 @@
+/**
+*	@file testclass.h
+*	@author Gustavo Assuncao
+*	@date 04/09/2018
+*	@brief A header file for the test class
+*/
+
+#ifndef TESTCLASS_H
+#define TESTCLASS_H
+
+class TestClass
+{
+	public:
+
+		
+
+
+	private:
+};
+
+#endif 
